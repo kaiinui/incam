96401ea7ce169328eea923263ded2c5e646b1eba
Kai INUI <lied.der.optik@gmail.com>
2014-08-16 21:01:12 +0900

    initial

---
+++ incam/KIViewController.h
@@ -0,0 +1,13 @@
+//
+//  KIViewController.h
+//  incam
+//
+//  Created by kaiinui on 2014/08/16.
+//  Copyright (c) 2014年 kaiinui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KIViewController : UIViewController
+
+@end
